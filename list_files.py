b6c5ed6a53fd16c1f0db686a2d3f4642f7574b86
Dylan <32645436+dyylanl@users.noreply.github.com>
2022-07-29 03:45:47 -0300

    Update list_files.py

--- list_files.py
+++ list_files.py
@@ -1,21 +1,31 @@
-from hashlib import new
 import os
 
+def scan(aut, path, extension):
+    try:
+        contenido = os.listdir(path)
+        files = []
+        index = 0
+        for file in contenido:
+            if (os.path.isfile(os.path.join(path, file))):
+                if (extension == "" or file.endswith(extension)): #este if se podria mejorar pero we
+                    if (aut == 'y'): #este tmb xdd
+                        new_name = ((file.split("."))[0]+str(index)+str(extension))
+                        os.rename(file, new_name)
+                        index += 1
+                    else:
+                        print('Nombre actual: ' + file)
+                        new_name = input('Ingrese el nuevo nombre: ')
+                        os.rename(file,new_name)
+    except(FileNotFoundError):
+        print('Error, ingreso una ruta invalida.')
+    except(KeyboardInterrupt):
+        print('')
+        print('Adios!')
 
-def scan(path, extension):
-    contenido = os.listdir(path)
-    files = []
-    index = 0
-    if (extension==""):
-        extension="*"
-    for file in contenido:
-        if (os.path.isfile(os.path.join(path, file)) and file.endswith(extension)):
-            res = input('Desea cambiar el nombre de ' +file+'? [y/n]: ')
-            if (res == 'y'):
-                new_name = input('Ingrese el nuevo nombre: ')
-                os.rename(file,new_name)
+def main():
+    res = input('Cambiar nombre automaticamente? [y/n]: ')
+    path = input('Ingrese la ruta de los archivos a procesar: ')
+    extension = input('Ingrese la extension (deje en blanco para procesar todas las extensiones): ')
+    scan(res, path, extension)
 
-
-path = input('Ingrese la ruta de los archivos a procesar: ')
-extension = input('Ingrese la extension: ')
-scan(path,extension)
+main()
